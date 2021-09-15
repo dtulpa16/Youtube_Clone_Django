a785cb556e5043a9884163510c2e2a397b2cc7e0
unknown <dtulpa16@yahoo.com>
2021-09-15 14:25:48 -0500

    Finished models

--- Youtube_Clone_Django_API/comments/models.py
+++ Youtube_Clone_Django_API/comments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Comment(models.Model):
+    video = models.CharField(max_length=100)
+    comment = models.CharField(max_length=1000)
+    likes = models.IntegerField(null=0)
+    dislikes=models.IntegerField(null=0)
 # Create your models here.
